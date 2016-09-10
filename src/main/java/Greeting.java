b52399fdc109b12c80f9a22ab82c115c6af2333a
Jonathan Leczner <jonathanleczner@Jonathan-ZCW-16.local>
2016-09-09 22:22:49 -0400

    project creation, first attempt at solution

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+/**
+ * Created by jonathanleczner on 9/9/16.
+ */
+public class Greeting {
+    public static void main(String[] args) {
+        Greeting g = new Greeting();
+        g.promptUser();
+        String inputName = g.getInput();
+        String greeting = g.greetingCheck(inputName);
+        g.printGreeting(greeting);
+    }
+
+    public void promptUser() {
+        System.out.println("Enter your name:");
+    }
+
+    public String getInput() {
+        Scanner s = new Scanner(System.in);
+        String name = s.next();
+        return name;
+    }
+    public String greetingCheck(String name) {
+        String greetingString = "";
+        switch (name) {
+            case "Bob":
+                greetingString = "Hey, Bob.";
+                break;
+            case "Alice":
+                greetingString = "Hey, Alice.";
+                break;
+            default:
+                break;
+        }
+        return greetingString;
+    }
+
+    public void printGreeting(String greeting) {
+        System.out.println(greeting);
+    }
+}
